025dbdce1db49324a14b0ae3e0a49e75065efeeb
irinalaptop <noem@mail.ru>
2024-09-12 12:22:10 +0300

    Fetching login and handling errors

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,175 +1,230 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import MuiCard from '@mui/material/Card';
-import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import MuiCard from "@mui/material/Card";
+import { ThemeProvider, createTheme, styled } from "@mui/material/styles";
+
+import { useNavigate } from "react-router-dom";
+import { baseurl } from "../shared";
+import { LoginContext } from "../App";
 
 const Card = styled(MuiCard)(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignSelf: 'center',
-  width: '100%',
+  display: "flex",
+  flexDirection: "column",
+  alignSelf: "center",
+  width: "100%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: 'auto',
-  [theme.breakpoints.up('sm')]: {
-    maxWidth: '450px',
+  margin: "auto",
+  [theme.breakpoints.up("sm")]: {
+    maxWidth: "450px",
   },
   boxShadow:
-    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-  ...theme.applyStyles('dark', {
+    "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
+  ...theme.applyStyles("dark", {
     boxShadow:
-      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+      "hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px",
   }),
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-  position: 'absolute',
-  left: '50%',
-  top: '50%',
-  transform: 'translate(-50%, -50%)',
-  height: '100%',
-  width: '100%' ,
+  position: "absolute",
+  left: "50%",
+  top: "50%",
+  transform: "translate(-50%, -50%)",
+  height: "100%",
+  width: "100%",
   padding: 20,
   backgroundImage:
-    'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-  backgroundRepeat: 'no-repeat',
-  ...theme.applyStyles('dark', {
+    "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+  backgroundRepeat: "no-repeat",
+  ...theme.applyStyles("dark", {
     backgroundImage:
-      'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+      "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))",
   }),
 }));
 
 export default function SignIn() {
-
-  const mode = 'light';
-  const defaultTheme = createTheme({ palette: { mode } });
+  const mode = "light";
+  const defaultTheme = createTheme({ palette: { mode },  });
 
   const [loginError, setLoginError] = React.useState(false);
-  const [loginErrorMessage, setLoginErrorMessage] = React.useState('');
+  const [loginErrorMessage, setLoginErrorMessage] = React.useState("");
   const [passwordError, setPasswordError] = React.useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
- 
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState("");
+  const [errorStatus, setErrorStatus] = React.useState();
+
+  const url = baseurl + "authentication/login";
+  const [loggedIn, setLoggedIn] = React.useContext(LoginContext);
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('login'),
-      password: data.get('password'),
-    });
+
+    if (loginError || passwordError) throw "vvedite barahlo";
+
+    const inputsValues = new FormData(event.currentTarget);
+    const userCredentials = {
+      username: inputsValues.get("login"),
+      password: inputsValues.get("password"),
+    };
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userCredentials),
+    })
+      .then((response) => {
+        if (response.status === 401) {
+          setErrorStatus(401);
+          console.log("incorrect info");
+          console.log(loggedIn);
+          throw response.status;
+        } else if (!response.ok) {
+          console.log("Other error we dont know");
+          throw response.status;
+        }
+        console.log("response passed");
+        return response.json();
+      })
+      .then((data) => {
+        console.log("success");
+        localStorage.setItem("accessToken", data.accessToken);
+        localStorage.setItem("refreshToken", data.refreshToken);
+        setLoggedIn(true);
+        /*  navigate(location?.state?.previousUrl ?
+          location.state.previousUrl : '/doccategories'
+       );*/
+        navigate("/documentstatuses");
+        console.log(localStorage);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
   };
 
   const validateInputs = () => {
-    const login = document.getElementById('login');
-    const password = document.getElementById('password');
+    const login = document.getElementById("login");
+    const password = document.getElementById("password");
 
     let isValid = true;
 
     if (!login.value) {
       setLoginError(true);
-      setLoginErrorMessage('Введите логин');
+      setLoginErrorMessage("Введите логин");
       isValid = false;
     } else {
       setLoginError(false);
-      setLoginErrorMessage('');
+      setLoginErrorMessage("");
     }
 
     if (!password.value) {
       setPasswordError(true);
-      setPasswordErrorMessage('Введите пароль');
+      setPasswordErrorMessage("Введите пароль");
       isValid = false;
     } else {
       setPasswordError(false);
-      setPasswordErrorMessage('');
+      setPasswordErrorMessage("");
     }
 
     return isValid;
   };
 
   return (
-      <ThemeProvider theme={defaultTheme}>
-        <CssBaseline enableColorScheme />
-        <SignInContainer direction="column" justifyContent="space-between">
-          <Card variant="outlined">
-            <Typography
-              component="h4"
-              variant="h4"
-              align='center'
-            >
-              АИС "Цифровая кафедра"
-            </Typography>
-            <Typography
-              component="h5"
-              variant="h5"
-              sx={{ width: '100%', fontSize: 'clamp(1rem, 10vw, 1.5rem)' }}
-            >
-              Вход
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: '100%',
-                gap: 2,
-              }}
-            >
-              <FormControl>
-                <FormLabel htmlFor="login">Логин</FormLabel>
-                <TextField
-                  error={loginError}
-                  helperText={loginErrorMessage}
-                  id="login"
-                  type="text"
-                  name="login"
-                  placeholder="логин"
-                  autoComplete="on"
-                  autoFocus
-                  required
-                  fullWidth
-                  variant="outlined"
-                  color={loginError ? 'error' : 'primary'}
-                  sx={{ ariaLabel: 'login' }}
-                />
-              </FormControl>
-              <FormControl>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <FormLabel htmlFor="password">Пароль</FormLabel>
-                </Box>
-                <TextField
-                  error={passwordError}
-                  helperText={passwordErrorMessage}
-                  name="password"
-                  placeholder="••••••"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  autoFocus
-                  required
-                  fullWidth
-                  variant="outlined"
-                  color={passwordError ? 'error' : 'primary'}
-                />
-              </FormControl>
-              <Button
-                type="submit"
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline enableColorScheme />
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <Typography component="h4" variant="h4" align="center">
+            АИС "Цифровая кафедра"
+          </Typography>
+          <Typography
+            component="h5"
+            variant="h5"
+            sx={{ width: "100%", fontSize: "clamp(1rem, 10vw, 1.5rem)" }}
+          >
+            Вход
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "100%",
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="login">Логин</FormLabel>
+              <TextField
+                error={loginError}
+                helperText={loginErrorMessage}
+                id="login"
+                type="text"
+                name="login"
+                placeholder="логин"
+                autoComplete="on"
+                autoFocus
+                required
                 fullWidth
-                variant="contained"
-                onClick={validateInputs}
-              >
-                Войти
-              </Button>
-            </Box>
-          </Card>
-        </SignInContainer>
-      </ThemeProvider>
+                variant="outlined"
+                color={loginError ? "error" : "primary"}
+                sx={{ ariaLabel: "login" }}
+              />
+            </FormControl>
+            <FormControl>
+              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                <FormLabel htmlFor="password">Пароль</FormLabel>
+              </Box>
+              <TextField
+                error={passwordError}
+                helperText={passwordErrorMessage}
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={passwordError ? "error" : "primary"}
+              />
+            </FormControl>
+
+           
+            {errorStatus === 401 ? (
+              <Typography sx={{ color: "red" }}>
+                Неверный логин или пароль
+              </Typography>
+            ) : null}
+
+            {errorStatus && errorStatus !== 401 ? (
+              <Typography sx={{ color: "red" }}>
+                Ошибка со стороны сервера
+              </Typography>
+            ) : null}
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={validateInputs}
+            >
+              Войти
+            </Button>
+          </Box>
+        </Card>
+      </SignInContainer>
+    </ThemeProvider>
   );
 }
